23553eae608fbae1a8c24f569e421e2d64eeb38a
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-03-18 10:24:03 +0200

    part 3 lesson 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { MainComponent } from './main/main.component';
+import { RecipesComponent } from './main/recipes/recipes.component';
+import { RecipeListComponent } from './main/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './main/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './main/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './main/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './main/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
