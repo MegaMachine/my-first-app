191376dfa339836cef60f26dceff55cceb852b2b
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-04-24 17:14:23 +0300

    WIP: part 6 lesson 3

--- src/app/main/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/main/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Input() recipe: Object;
   constructor() { }
-
   ngOnInit() {
   }
 
