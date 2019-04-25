797050d73dc7f2060a117432700e7d79bd4b9864
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-04-25 16:21:31 +0300

    part 6 lesson 4

--- src/app/main/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/main/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {SelectItem} from 'primeng/api';
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
   selectOption: string;
   optionsGroup: SelectItem[];
   constructor() {
     this.optionsGroup = [
       {
         label: 'To Shopping List',
-        value:'1'
+        value: '1'
       },
       {
         label: 'Edit Recipe',
-        value:'1'
+        value: '1'
       },
       {
         label: 'To Shopping List',
-        value:'1'
+        value: '1'
       }
-    ]
+    ];
   }
 
   ngOnInit() {
--- src/app/main/recipes/recipe-list/recipe-list.component.ts
+++ src/app/main/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from './../recipe.model';
 
 @Component({
@@ -7,26 +7,30 @@ import { Recipe } from './../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test recipe',
-      'Test',
+      'Test1',
       'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
     ),
     new Recipe(
       'A Test recipe',
-      'Test',
+      'Test2',
       'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
     ),
     new Recipe(
       'A Test recipe',
-      'Test',
+      'Test3',
       'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
     )
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
