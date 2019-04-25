8b67c6796a2a545c8cd0f14184e5949d11468196
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-04-25 16:48:28 +0300

    part 6 lesson 6

--- src/app/main/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/main/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent implements OnInit {
-  count = 0;
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
-  testDeug(){
-    this.count++;
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient( ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 }
