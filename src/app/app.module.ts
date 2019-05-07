8900f70fae5396fe1e6ab56e6e3ec1017542f828
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-07 14:57:09 +0300

    part 7 lesson 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { BasicHighlightDirective } from './basic-highlights/basic-highlight.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
