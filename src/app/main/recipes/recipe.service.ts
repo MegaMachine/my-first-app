09d689229e88069b2038d75646ff3d2d0ea0ef9c
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-13 16:13:39 +0300

    part 10 lesson 7

--- src/app/main/recipes/recipe.service.ts
+++ src/app/main/recipes/recipe.service.ts
@@ -1,23 +1,35 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test recipe',
+      'Sendwitch',
       'Test1',
-      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
+      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Breat', 1)
+      ]
     ),
     new Recipe(
-      'A Test recipe',
+      'Fry Potatos',
       'Test2',
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/09/1519654520-delish-glazed-carrots-1.jpg?crop=1.0xw:1xh;center,top&resize=480:*'
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/09/1519654520-delish-glazed-carrots-1.jpg?crop=1.0xw:1xh;center,top&resize=480:*',
+      [
+        new Ingredient('Potato', 5),
+        new Ingredient('Oil', 1)
+      ]
     ),
     new Recipe(
-      'A Test recipe',
+      'Chiken',
       'Test3',
-      'https://cdn.vox-cdn.com/thumbor/haPXSPnaeo-UIsaM-G3WLvNIUgc=/0x0:2400x1603/1200x675/filters:focal(1008x610:1392x994)/cdn.vox-cdn.com/uploads/chorus_image/image/58071251/NS019342.0.jpg'
+      'https://cdn.vox-cdn.com/thumbor/haPXSPnaeo-UIsaM-G3WLvNIUgc=/0x0:2400x1603/1200x675/filters:focal(1008x610:1392x994)/cdn.vox-cdn.com/uploads/chorus_image/image/58071251/NS019342.0.jpg',
+      [
+        new Ingredient('Chiken', 1)
+      ]
     )
   ];
   getRecipes() {
