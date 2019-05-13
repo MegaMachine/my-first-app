6c2c230c4f23a88906eb5be51998af61af5ca0b9
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-13 14:32:44 +0300

    part 10 lesson 3

--- src/app/main/recipes/recipe-list/recipe-list.component.ts
+++ src/app/main/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from './../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,27 +9,13 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test recipe',
-      'Test1',
-      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
-    ),
-    new Recipe(
-      'A Test recipe',
-      'Test2',
-      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
-    ),
-    new Recipe(
-      'A Test recipe',
-      'Test3',
-      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
-    )
-  ];
-  constructor() { }
+  recipes: Recipe[];
+  constructor( private recipeService: RecipeService) {}
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
+
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
   }
--- src/app/main/recipes/recipe.service.ts
+++ src/app/main/recipes/recipe.service.ts
@@ -1,3 +1,23 @@
+import { Recipe } from './recipe.model';
 export class RecipeService {
-
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test recipe',
+      'Test1',
+      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'
+    ),
+    new Recipe(
+      'A Test recipe',
+      'Test2',
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/18/09/1519654520-delish-glazed-carrots-1.jpg?crop=1.0xw:1xh;center,top&resize=480:*'
+    ),
+    new Recipe(
+      'A Test recipe',
+      'Test3',
+      'https://cdn.vox-cdn.com/thumbor/haPXSPnaeo-UIsaM-G3WLvNIUgc=/0x0:2400x1603/1200x675/filters:focal(1008x610:1392x994)/cdn.vox-cdn.com/uploads/chorus_image/image/58071251/NS019342.0.jpg'
+    )
+  ];
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
