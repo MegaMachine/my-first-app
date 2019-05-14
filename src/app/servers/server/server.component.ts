344e43a4f7081ad48fa5743fed555b38c45ad930
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-14 16:49:25 +0300

    part 11 lessont 18

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -22,5 +23,9 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe( (params: Params) => {
       this.server = this.serversService.getServer( +params.id) ;
     });
+  }
+  onEdit() {
+    console.log(this.route);
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
