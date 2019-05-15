07c2a9f98fa095a9a5152a1a49d1fa54720fcec9
liquidum <tolikdanylyuk@gmail.com>
2019-05-15 17:15:53 +0300

    part 11 lesson 25

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,14 +1,15 @@
 import { AuthService } from './auth.service';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivate, CanActivateChild } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(
     private authService: AuthService,
     private router: Router
   ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -22,5 +23,12 @@ export class AuthGuard implements CanActivate {
           return false;
         }
       });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate( route, state );
   }
 }
