8fb615aec73bd287e191aba34a716b4bb6d35cfd
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-20 15:06:41 +0300

    part 13 lesson 5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
 @Component({
   selector: 'app-home',
@@ -11,10 +12,29 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe((numb: number) => {
-      console.log(numb);
-    })
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe((numb: number) => {
+    //   console.log(numb);
+    // })
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.error('third package');
+      }, 6000);
+    });
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('Completed'); }
+    );
   }
 
 }
