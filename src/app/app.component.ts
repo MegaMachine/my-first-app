c5096ca524c9334915ff3f82f50f531645f068f0
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-05-22 11:10:29 +0300

    part 15 lesson 23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   singupForm: FormGroup;
+
+  ngOnInit() {
+    this.singupForm = new FormGroup({
+      'username': new FormControl( null ),
+      'email': new FormControl( null ),
+      'gender': new FormControl( 'male' )
+    });
+  }
 }
