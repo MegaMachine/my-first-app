4f08792e751156f5bc2ac7799c4b50bbab1ba44b
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-06-18 16:37:06 +0300

    part 21 lessons 1,2,3,4,5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { RecipesModule } from './main/recipes/recipes.module';
 import { AuthGuard } from './auth/auth-guard.service';
 import { AuthService } from './auth/auth.service';
-import { HttpClientModule } from '@angular/common/http';
 import { RecipeService } from './main/recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListService } from './main/shopping-list/shopping-list.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {DropdownModule} from 'primeng/dropdown';
-
+import { DropdownModule } from 'primeng/dropdown';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { RecipesComponent } from './main/recipes/recipes.component';
-import { RecipeListComponent } from './main/recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './main/recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './main/recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './main/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './main/shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { RecipeStartComponent } from './main/recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './main/recipes/recipe-edit/recipe-edit.component';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -31,15 +26,9 @@ import { SigninComponent } from './auth/signin/signin.component';
     AppComponent,
     HeaderComponent,
     MainComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
@@ -49,8 +38,8 @@ import { SigninComponent } from './auth/signin/signin.component';
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
