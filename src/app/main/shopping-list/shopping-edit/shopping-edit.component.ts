6a185ee8b4487c331288af441aa26f8ec9744d62
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-06-21 16:07:17 +0300

    part 24 lesson 11

--- src/app/main/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/main/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,9 +37,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient( value.name, value.amount);
     if (this.editMode) {
-      this.slService.updateIngredient( this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient}));
+
     } else {
-      // this.slService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
     this.editMode = false;
@@ -51,7 +51,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
   onDelete() {
     if (this.editMode) {
-      this.slService.deleteIngredient(this.editedItemIndex);
+      this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex));
       this.onClear();
     }
   }
--- src/app/main/shopping-list/store/shopping-list.reducers.ts
+++ src/app/main/shopping-list/store/shopping-list.reducers.ts
@@ -9,7 +9,7 @@ const initialState = {
 };
 
 export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
-  console.log(state);
+  console.log({...state});
   console.log(action);
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
@@ -21,7 +21,27 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
       return {
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
-      }
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const ingredients = [...state.ingredients];
+      ingredients[action.payload.index] = updatedIngredient;
+      console.log(updatedIngredient);
+      return {
+        ...state,
+        ingredients: ingredients
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      const oldIngredients = [...state.ingredients];
+      oldIngredients.splice(action.payload, 1);
+      return {
+        ...state,
+        ingredients: oldIngredients
+      };
     default:
       return state;
   }
