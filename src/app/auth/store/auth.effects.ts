ba08e05ff909fff49e9b57abf8805c6413bfa9e1
Anatoliy Danylyuk <anatoliy.danylyuk@bytebrand.net>
2019-06-25 19:08:50 +0300

    part 24 lesson 28

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,36 @@
+import { map, switchMap, mergeMap } from 'rxjs/operators';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import * as AuthAction from './auth.actions';
+import { from } from 'rxjs';
+import * as firebase from 'firebase';
+import * as AuthActions from './auth.actions';
 @Injectable()
 export class AuthEffects {
-  @Effect()
-  authSignup = this.actions$.pipe(ofType(AuthAction.TRY_SIGNUP));
+  @Effect() //{dispatch: false}
+  authSignup = this.actions$
+    .pipe(
+      ofType(AuthActions.TRY_SIGNUP),
+      map((action: AuthActions.TrySignup) => {
+        return action.payload;
+      }),
+      switchMap((authData: {username: string, password: string}) => {
+        return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+      }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken(true));
+      }),
+      mergeMap((token: string) => {
+        return [
+          {
+            type: AuthActions.SIGNUP
+          },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          }
+        ];
+      })
+    );
   constructor(
     private actions$: Actions
   ) {}
